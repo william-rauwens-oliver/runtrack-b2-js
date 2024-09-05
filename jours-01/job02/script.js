e2c558e00596eea9146425ab10abc2e7dbecdf47
william-rauwens-oliver <william.rauwens-oliver@laplateforme.io>
2024-09-05 09:49:09 +0200

    Add Job02.

---
+++ jours-01/job02/script.js
@@ -0,0 +1,10 @@
+function myCountChar(haystack, needle) {
+    var count = 0;
+    for (var i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(myCountChar("hello World", "o") === 2);
